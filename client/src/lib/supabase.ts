4d311c5d871669900eef59d3cc63d4f8e5dafbf3
Radwan263 <Rowanradwan263@gmail.com>
2026-01-13 08:07:13 +0200

    Update supabase.ts

--- client/src/lib/supabase.ts
+++ client/src/lib/supabase.ts
@@ -4,6 +4,6 @@ const supabaseUrl = 'https://wiosxbjabfcxwevtyojj.supabase.co'
 
 // ❌ إياك تحط sb_publishable هنا تاني
 // ✅ حط المفتاح الطويل جداً اللي بيبدأ بـ eyJh هنا بين علامتين التنصيص
-const supabaseAnonKey = 'انسخ_المفتاح_الطويل_اللي_بيبدا_بـ_eyJh_وحطه_هنا'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indpb3N4YmphYmZjeHdldnR5b2pqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgyNTg5MDgsImV4cCI6MjA4MzgzNDkwOH0.A_nbC7ImuUY5b7bTiJl6m-5Z86Euo9euAeqtCp9oeYg'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
